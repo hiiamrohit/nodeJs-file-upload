e7e46a9251414d604d5f5268ab413edbc4193b10
Rohit <rohit.php5@gmail.com>
2014-07-30 10:13:02 +0530

    changed port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var fs = require('fs');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
